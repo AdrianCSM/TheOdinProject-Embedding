f97a660c21228a447e827419a302b239bf4d2656
AdrianCSM <90456900+AdrianCSM@users.noreply.github.com>
2022-01-23 02:39:08 +0200

    Files added via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+Embedding Videos and Images
+
+
+Sources : 
+
+Icons :  Material Design Icons https://materialdesignicons.com/
+Fluid Width : Video CSS-TRICKS https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+Line Clamp : CSS-TRICKS https://css-tricks.com/almanac/properties/l/line-clamp/
---
+++ app.js
@@ -0,0 +1,11 @@
+const readMoreBtn = document.querySelector(".btn.show-more-btn");
+const text = document.querySelector(".description");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
