60205323365f64192fd66a5b27601fc54d058a39
Cosmin Adrian <cosminadrian218@gmail.com>
2022-02-08 09:49:37 +0200

    Changed JS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const readMoreBtn = document.querySelector(".btn.show-more-btn");
 const text = document.querySelector(".description");
 
 readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
+  text.classList.toggle("moreText");
   if (readMoreBtn.innerText === "Read More") {
     readMoreBtn.innerText = "Read Less";
   } else {
